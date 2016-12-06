0f787a349be68e26309dcc305080e8fd98eb286e
xfw19910110 <xfw@mail.ustc.edu.cn>
2016-12-06 11:06:10 +0800

    using solc@0.4.4

--- app.js
+++ app.js
@@ -2,43 +2,7 @@
 var web3 = new Web3(new Web3.providers.HttpProvider("http://" + window.location.hostname + ":8545"));
 
 // solc --abi coin.sol 
-var abi = [{
-  "constant": true,
-  "inputs": [{
-    "name": "",
-    "type": "address"
-  }],
-  "name": "balances",
-  "outputs": [{
-    "name": "",
-    "type": "uint256"
-  }],
-  "type": "function"
-}, {
-  "constant": false,
-  "inputs": [{
-    "name": "amount",
-    "type": "uint256"
-  }],
-  "name": "generate",
-  "outputs": [],
-  "type": "function"
-}, {
-  "constant": false,
-  "inputs": [{
-    "name": "receiver",
-    "type": "address"
-  }, {
-    "name": "amount",
-    "type": "uint256"
-  }],
-  "name": "transfer",
-  "outputs": [],
-  "type": "function"
-}, {
-  "inputs": [],
-  "type": "constructor"
-}];
+var abi = [{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balances","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"generate","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"receiver","type":"address"},{"name":"amount","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"type":"function"},{"inputs":[],"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"amount","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"amount","type":"uint256"},{"indexed":false,"name":"success","type":"bool"}],"name":"Transfer","type":"event"}];
 
 // 使用node deploy.js返回的地址
 var Coin = web3.eth.contract(abi).at('0x8b545a42bf8b9748ba6408209b733e28a42ce5cb');
@@ -71,6 +35,8 @@ var app = new Vue({
       console.log("Transfer " + this.amount + " from " + this.sender + " to " + this.receiver);
       Coin.transfer(this.receiver, this.amount, {
         from: this.sender
+      }, function(err, tx) {
+	  console.log(tx);
       });
     },
 
--- coin.sol
+++ coin.sol
@@ -1,3 +1,5 @@
+pragma solidity ^0.4.4;
+
 contract Coin {
   // 管理员地址
   address admin;
